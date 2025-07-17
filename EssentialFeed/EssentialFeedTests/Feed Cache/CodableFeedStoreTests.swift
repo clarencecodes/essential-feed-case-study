12af03338fe1c18bbcd7c194f6454d9272ec0978
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2025-07-17 20:51:43 +0800

    Retrieving from non-empty cache twice delivers same found result (no side-effects)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -140,6 +140,37 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_retrieveHasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            
+            sut.retrieve { firstResult in
+                sut.retrieve { secondResult in
+                    switch (firstResult, secondResult) {
+                    case let (.found(firstFeedResult, firstTimestampResult), .found(secondFeedResult, secondFeedTimestamp)):
+                        XCTAssertEqual(firstFeedResult, feed)
+                        XCTAssertEqual(firstTimestampResult, timestamp)
+                        
+                        XCTAssertEqual(secondFeedResult, feed)
+                        XCTAssertEqual(secondFeedTimestamp, timestamp)
+                        
+                    default:
+                        XCTFail("Expected retrieving twice from non-empty cache to deliver same found result with feed \(feed) and timestamp \(timestamp), got \(firstResult) and \(secondResult) instead")
+                    }
+                    
+                    exp.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
