a5f2c097e5f5a1e323ae01e1e1afb813a4d45332
Clarence Chan <38497249+clarencecodes@users.noreply.github.com>
2025-11-02 15:22:09 +0800

    Replace custom `RetrieveCacheFeedResult` enum with a nested typealias over the standard `Swift.Result` (`FeedStore.RetrievalResult`)

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableInsertFeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -25,6 +25,6 @@ extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
         
         insert((feed.local, timestamp), to: sut)
         
-        expect(sut, toRetrieve: .empty)
+        expect(sut, toRetrieve: .success(.empty))
     }
 }
